e73a913e21e5c59af2e64c05a4eef34aae921216
Tanya777-cloud <71553193+Tanya777-cloud@users.noreply.github.com>
2022-07-26 00:11:54 +0300

    Add files via upload

---
+++ ClickTrigger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTrigger : MonoBehaviour
+{
+	TicTacToeAI _ai;
+
+	[SerializeField]
+	private int _myCoordX = 0;
+	[SerializeField]
+	private int _myCoordY = 0;
+
+	[SerializeField]
+	private bool canClick;
+
+	private void Awake()
+	{
+		_ai = FindObjectOfType<TicTacToeAI>();
+	}
+
+	private void Start(){
+
+		_ai.onGameStarted.AddListener(AddReference);
+		_ai.onGameStarted.AddListener(() => SetInputEndabled(true));
+		_ai.onPlayerWin.AddListener((win) => SetInputEndabled(false));
+	}
+
+	private void SetInputEndabled(bool val){
+		canClick = val;
+	}
+
+	private void AddReference()
+	{
+		_ai.RegisterTransform(_myCoordX, _myCoordY, this);
+		canClick = true;
+	}
+
+	private void OnMouseDown()
+	{
+		if(canClick){
+			_ai.PlayerSelects(_myCoordX, _myCoordY);
+			AiSelects();
+		}
+	}
+
+	private void AiSelects()
+	{
+		int aiCoordX = _myCoordX;
+		int aiCoordY = _myCoordY;
+		bool isSelected = false;
+
+		for (int y = 0; y <= 2; y++)
+		{
+			if (!isSelected)
+			{
+				for (int x = 0; x <= 2; x++)
+				{
+					if (!_ai.IsSelected(x, y))
+					{
+						if (x == _myCoordX || y == _myCoordY)
+						{
+							aiCoordX = x;
+							aiCoordY = y;
+							isSelected = true;
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		if (isSelected)
+		{
+			_ai.AiSelects(aiCoordX, aiCoordY);
+		}
+	}
+}
---
+++ TicTacToeAI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum TicTacToeState{none, cross, circle}
+
+[System.Serializable]
+public class WinnerEvent : UnityEvent<int>
+{
+}
+
+public class TicTacToeAI : MonoBehaviour
+{
+
+	int _aiLevel;
+
+	TicTacToeState[,] _boardState;
+
+	[SerializeField]
+	private bool _isPlayerTurn;
+
+	[SerializeField]
+	private int _gridSize = 3;
+	
+	//[SerializeField]
+	TicTacToeState _playerState = TicTacToeState.cross;
+	TicTacToeState _aiState = TicTacToeState.circle;
+
+	[SerializeField]
+	private GameObject _xPrefab;
+
+	[SerializeField]
+	private GameObject _oPrefab;
+
+	public UnityEvent onGameStarted;
+
+	//Call This event with the player number to denote the winner
+	public WinnerEvent onPlayerWin;
+
+	ClickTrigger[,] _triggers;
+	
+	private void Awake()
+	{
+		if(onPlayerWin == null){
+			onPlayerWin = new WinnerEvent();
+		}
+	}
+
+	public void StartAI(int AILevel){
+		_aiLevel = AILevel;
+		StartGame();
+	}
+
+	public void RegisterTransform(int myCoordX, int myCoordY, ClickTrigger clickTrigger)
+	{
+		_triggers[myCoordX, myCoordY] = clickTrigger;
+	}
+
+	private void StartGame()
+	{
+		_triggers = new ClickTrigger[_gridSize, _gridSize];
+		_boardState = new TicTacToeState[_gridSize, _gridSize];
+		onGameStarted.Invoke();
+	}
+
+	public void PlayerSelects(int coordX, int coordY){
+
+		SetVisual(coordX, coordY, _playerState);
+	}
+
+	public void AiSelects(int coordX, int coordY){
+
+		SetVisual(coordX, coordY, _aiState);
+	}
+
+	private void SetVisual(int coordX, int coordY, TicTacToeState targetState)
+	{
+		Instantiate(
+			targetState == TicTacToeState.circle ? _oPrefab : _xPrefab,
+			_triggers[coordX, coordY].transform.position,
+			Quaternion.identity
+		);
+
+		_boardState[coordX, coordY] = targetState;
+	}
+
+	public bool IsSelected(int coordX, int coordY)
+	{
+		return _boardState[coordX, coordY] != TicTacToeState.none; 
+	}
+}
